ba45b60752b781faf95468776865fbf4f1125091
Joel <joel.goldstick.com>
2016-01-14 12:35:25 -0500

    modified:   blog/settings.py modified:   blog/urls.py modified:   blog_app/__init__.py modified:   blog_app/models.py new file:   my_blog/__init__.py new file:   my_blog/admin.py new file:   my_blog/apps.py new file:   my_blog/forms.py new file:   my_blog/migrations/0001_initial.py new file:   my_blog/migrations/__init__.py new file:   my_blog/models.py new file:   my_blog/tests.py new file:   my_blog/urls.py new file:   my_blog/views.py Created new app called my_blog to try to sort out why migrations didn't work for blog_app Finally got migrations to work.  I think it had to do with stale migrations file.  It seemed to think the tables were already in the db but they weren't

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, include, url
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin, admindocs
 
-import blog_app
+#import blog_app
+import my_blog
 admin.autodiscover()
 
 urlpatterns = [
@@ -12,8 +13,7 @@ urlpatterns = [
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^blog/', include('blog_app.urls')) ,
-    url(r'^user/', include('user_app.urls')),
-    #url(r'^', include(blog.blog_app.urls)),
+    #url(r'^blog/', include('blog_app.urls')) ,
+    url(r'^blog/', include('my_blog.urls')) ,
 ]
 
---
+++ blog/my_blog/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, url, include
+#from blog_app import views
+from my_blog import views
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [ 
+    url(r'^$', views.entries_index, name="index"),
+    url(r'^(?P<year>\d{4})/(?P<month>\w{3})/(?P<day>\d{2})/(?P<slug>[-\w]+)/$', views.entry_detail),
+    url(r'^categories/$', views.category_list),
+    url(r'^categories/(?P<category>\w+)/$', views.entries_by_category),
+    url(r'^contact/$', views.contact),
+    url(r'^feed/latest/$', views.LatestEntriesFeed()),
+    url(r'^cbv/list/$', views.EntryList.as_view()),
+    url(r'^cbv/create/$', login_required(views.EntryCreate.as_view())),
+    url(r'^cbv/update/(?P<pk>\d+)/$', login_required(views.EntryUpdate.as_view())),
+    url(r'^cbv/delete$', views.EntryList.as_view()),
+]
