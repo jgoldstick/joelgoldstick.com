03c2c1541e9728439a3676439e9e28e894540a33
Joel Goldstick <joel.goldstick@gmail.com>
2016-03-29 10:16:06 -0400

    added url to access blog from joelgoldstick.com directly using redirect

--- blog/blog/urls.py
+++ blog/blog/urls.py
@@ -1,19 +1,17 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import include, url
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin, admindocs
+from . import views
 
-import blog_app
 admin.autodiscover()
 
-urlpatterns = patterns('',
-
+urlpatterns = [
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include(admindocs.urls)),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^blog/', include('blog_app.urls')) ,
+    url(r'^$', views.blog),
+    url(r'^blog/', include('blog_app.urls')),
     url(r'^user/', include('user_app.urls')),
-    #url(r'^', include(blog.blog_app.urls)),
-)
-
+]
---
+++ blog/blog/views.py
@@ -0,0 +1,6 @@
+# Create your views here.
+from django.shortcuts import redirect
+
+
+def blog(request):
+    return redirect('/blog/')
