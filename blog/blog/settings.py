4f8dd98e601308cec0be12f1d36cc7d6243009ca
Joel Goldstick <joel.goldstick@gmail.com>
2016-03-29 11:06:56 -0400

    fixed typo from previous merge conflict

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -27,7 +27,6 @@ def get_env_variable(var_name):
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-<<<<<<< HEAD
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR,  'templates'),
 )
